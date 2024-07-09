9117500ddfe1004e10206139e143b60552089a26
BharathKatabathini <katabathini.bharath2000@gmail.com>
2024-07-09 18:10:06 +0530

    ArrayList impelemnted

---
+++ collections.java
@@ -0,0 +1,21 @@
+public class Collections{
+//LinkedList Structure
+public class LinkedList{
+private Node head;	
+private class Node{
+private int data;
+private Node next;
+public Node(int data){
+this.data=data;
+this.next=null;
+}
+
+} 
+
+
+}
+
+
+
+
+}
